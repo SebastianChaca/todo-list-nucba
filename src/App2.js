b0406e7ff889e90ccea4acf4a7edddd8e3888a8e
Sebastian Chaca <sebastian.dalinger@gmail.com>
2021-06-28 20:45:17 -0300

    todolist

--- src/App2.js
+++ src/App2.js
@@ -4,51 +4,71 @@ import { Button, Flex, Input, Text, Box, Spacer  } from "@chakra-ui/react"
 import {AiFillDelete} from 'react-icons/ai'
 import {MdModeEdit} from 'react-icons/md'
 const App2 = () => {
-  const reducer=(state, action)=>{
-    switch (action.type){
-      case 'PARSE_INPUT':
-        return {...state, item:action.payload}
-      case 'ADD_ITEM':
-        return{...state, lista:[...state.lista, state.item ], item:{texto:'', id:''}}
-      case 'DELETE_ITEM':
-        return{...state, lista:action.payload}
+  const reducer =(state, action)=>{
+    switch (action.type) {
+      case 'HANDLE_CHANGE':
+        if(state.modoEdicion){
+          return {...state, tareaEditar:{...state.tareaEditar, texto: action.payload}}
+        }else{
+          return {...state, tareaInput: action.payload}
+
+        }
+      case 'ADD_TODO':
+        return {...state, lista:[...state.lista, {texto:state.tareaInput, id:uuidv4()}], tareaInput:''}
+      case 'DELETE_TODO':
+        return {...state, lista: action.payload}
       case 'EDIT_MODE':
-        return {...state, editMode: {editState: action.payload.editState, tareaEditar: action.payload.tareaEditar}}
+        return {...state, modoEdicion: !state.modoEdicion, tareaEditar:action.payload, tareaInput:''}
+      case 'TODO_EDITED':
+        const newTodoEdited= state.lista.map((valor)=>
+         valor.id === state.tareaEditar.id ? state.tareaEditar : valor
+        )
+        return {...state, lista:newTodoEdited, tareaEditar:null, modoEdicion: !state.modoEdicion}
       default:
         return state
     }
   }
-  let initialState={lista:[], item:{texto:'', id:null}, editMode:{editState:false, tareaEditar:''}}
+  const initialState={
+    lista:[],
+    tareaInput:'',
+    modoEdicion:false,
+    tareaEditar:null,
+    errorState:''
+  }
   const [state, dispatch]=useReducer(reducer, initialState)
+  console.log(state)
   
+  const handleOnChange=(e)=>{    
 
-  
-
-  const handleInput=(e)=>{
-    dispatch({type:'PARSE_INPUT', payload:{texto:e.target.value, id:uuidv4()}})
+      dispatch({type:'HANDLE_CHANGE', payload: e.target.value})
+    
   }
   const handleSubmit=(e)=>{
     e.preventDefault()
-    dispatch({type:'ADD_ITEM'})
+    if (state.modoEdicion){
+      dispatch({type:'TODO_EDITED'})
+    }else{
 
+      dispatch({type:'ADD_TODO'})
+    }
   }
   const handleDelete=(id)=>{
-    const newList= state.lista.filter((item => item.id !== id ))
-    dispatch({type:'DELETE_ITEM', payload:newList})
+    const newTodo= state.lista.filter((item)=> item.id !== id)
+    dispatch({type:'DELETE_TODO', payload: newTodo})
   }
-  const handleEditMode=(item)=>{    
-    dispatch({type:'EDIT_MODE', payload: {editState:!state.editMode.editState, tareaEditar: item.texto}})
+  const handleEdit=(item)=>{
+    dispatch({type:'EDIT_MODE', payload:item})
   }
-console.log(state)
+ 
   return (
     <>
     <Flex justifyContent='space-around' mt='30px' >
     <Box w='300px'>
     <Text textAlign='center'>Escribí algo :</Text>
       <form onSubmit={handleSubmit}>      
-        <Input onChange={handleInput} value={state.editMode.editState ? state.editMode.tareaEditar :state.item.texto} type='text'/>
+        <Input onChange={handleOnChange} value={state.modoEdicion ? state.tareaEditar.texto : state.tareaInput} type='text'/>
         <Button w='100%' mt='10px' color='white' bg='blue.800' type='submit'>
-          {state.editMode ? 'Editar' : 'Agregar'}
+           {state.modoEdicion ? 'Editar' : 'Agregar'}
         </Button>
       </form>
     </Box>
@@ -59,7 +79,7 @@ console.log(state)
           return <Flex 
                   justifyContent='center' 
                   alignItems='center'
-                  w='600px' 
+                  w={{base: '300px', sm:'300px', md:'500px', lg:'500px'}}
                   mb='20px' 
                   border="1px" 
                   borderColor="gray.200" 
@@ -70,8 +90,8 @@ console.log(state)
           
            <Text ml='5px'>{item.texto}</Text> 
            <Spacer/>
-           <Button mx='5px' bg='red.400' onClick={()=> handleDelete(item.id)}> <AiFillDelete/> </Button>
-           <Button mx='5px' bg='green.300' onClick={()=> handleEditMode(item)}> <MdModeEdit/> </Button>  
+           <Button mx='5px' bg='red.400' onClick={()=> handleDelete(item.id)} > <AiFillDelete/> </Button>
+           <Button mx='5px' bg='green.300' onClick={()=>handleEdit(item)}> <MdModeEdit/> </Button>  
            </Flex>
         }) : <Text>No hay Tareas</Text>
       }
